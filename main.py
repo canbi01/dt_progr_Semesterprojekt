e1596f3f350b8972fd955cc76321a90122fa7d9c
canbi01 <ben.wild@stud.hslu.ch>
2024-12-02 17:53:57 +0100

    Ordner

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from archicad import ACConnection
 from typing import List, Tuple, Iterable
 
 # Interface
-import interface_final
+import Prozessor.interface_final as inter
 
 if __name__ == "__main__":
-    interface_final.start_app()
+    inter.start_app()
 
 # Verbindung zu Archicad herstellen
 conn = ACConnection.connect()
